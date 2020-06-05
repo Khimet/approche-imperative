a6a906917e656c2df19eed9067dbc6a5be1e9674
Khalil HIMET <khalil.himet@outlook.com>
2020-06-05 15:26:40 +0200

    1er commit du TP2

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,11 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Bonjour !");
+
+	}
+
+}
