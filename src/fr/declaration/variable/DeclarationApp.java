10cde052340723fcf84ac4abdc1f042b710bd276
Khalil HIMET <khalil.himet@outlook.com>
2020-06-05 17:06:04 +0200

    Commit du TP2 terminé

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -4,7 +4,32 @@ public class DeclarationApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Bonjour !");
+		System.out.println("Test Bonjour TP2 !");
+		
+		byte petitNombre = 2;
+		short petitMoyenNombre = 3;
+		int nombreEntier = 4;
+		long grandNombre = 5L;
+		float nombreDecimal = 6f;
+		double grandNombreDecimal = 7; // utilisé par defaut pr les nombres décimaux
+		char caractereUnique = 'x';
+		boolean valeurBooleene = true;
+		String message = "Bonjour !";
+		
+		//Affichage de la valeur d'une variable
+		System.out.println(grandNombre);
+		
+		//Retour à la ligne dans une chaine de caractere
+		String randomString = "Voici le résultat d’un calcul : \n 1+5=6";
+		
+		System.out.println(randomString);
+		
+		
+		
+		
+				
+		
+		
 
 	}
 
